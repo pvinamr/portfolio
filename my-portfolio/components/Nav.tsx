22bdd41609705d3dbeba6410cd7612091e1eec8b
Vinamr Pemmaraju <pvinamr@gmail.com>
2026-01-06 16:43:20 -0500

    nav bar + footer

---
+++ my-portfolio/components/Nav.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+export default function Nav() {
+  return (
+    <header style={{ padding: "32px 0" }}>
+      <nav style={{ display: "flex", justifyContent: "space-between", gap: 16 }}>
+        <Link href="/" style={{ textDecoration: "none", fontWeight: 600 }}>
+          Your Name
+        </Link>
+
+        <div style={{ display: "flex", gap: 14, fontSize: 14 }}>
+          <Link href="/projects" style={{ textDecoration: "none" }}>Projects</Link>
+          <Link href="/resume" style={{ textDecoration: "none" }}>Resume</Link>
+          <a href="https://github.com/pvinamr" target="_blank" rel="noreferrer" style={{ textDecoration: "none" }}>
+            GitHub
+          </a>
+          <a href="https://www.linkedin.com/in/vinamr-pemmaraju-056001225/" target="_blank" rel="noreferrer" style={{ textDecoration: "none" }}>
+            LinkedIn
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
